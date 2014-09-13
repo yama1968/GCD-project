fff582958196f97e66fb7e2eaa226c64c2f00f4e
yama1968 <ymartel@gmail.com>
2014-09-13 18:16:51 +0200

    2nd working version

--- README.md
+++ README.md
@@ -3,6 +3,29 @@ title: "README"
 output: html_document
 ---
 
-This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
+All the work is performed by the run_analysis.R script.
 
+Where:
+- you should run it with working file the directory where 'UCI HAR Dataset' extracted main directory is located
+
+What, steps 1, 2, 3, 4:
+- the script begins with reading each of training and testing, reading subject id, label and the 6 variables, means and standard deviation of movements along X, Y, Z axis
+- then it merge them into a single data set
+- and then get it into the tidyDataComplete data frame variable
+
+What, step 5:
+- the script then compute the summary data set
+- write it to the 'summary.txt' file
+- and then get the tidyDataSummary summary data frame variable
+
+How:
+- just run the script
+
+Input parameters
+- none
+
+Outputs:
+- tidyDataComplete: complete data set as data frame
+- tidyDataSummary: summary data set with averages
+- 'summary.txt' csv file, produce in working directory, and can be read back with read.csv('summary.txt')
 
--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,17 @@
 # Main analysis script for GDC Course Project
 #
 
+# location of data = name of directory
 dataset ="UCI HAR Dataset"
+
+# read one sample set of data, whether "train" or "test"
+# Output: data frame of sample data, with columns
+#   subject: subject id performing activity
+#   activity: activity label as factor, for the activity being performed
+#   meanX, meanY, meanZ, stdX, stdY, stdZ: means and standard deviations for X, Y, Z axis, during period of each sample
+# Inputs:
+#   source: source set, whether "train" or "test"
+#   n: optional, number of samples to read from files, or -1 for all. Defaults to all
 
 readOneSample = function (source, n=-1) {
         df = read.fwf(paste(dataset, '/', source,'/X_', source, '.txt', sep=''),
@@ -25,12 +35,27 @@ readOneSample = function (source, n=-1) {
         df
 }
 
+# read both set of data, "train" or "test", and merge them into a single data set
+# Output: data frame for train and set data, with columns
+#   subject: subject id performing activity
+#   activity: activity label as factor, for the activity being performed
+#   meanX, meanY, meanZ, stdX, stdY, stdZ: means and standard deviations for X, Y, Z axis, during period of each sample
+# Inputs:
+#   n: optional, number of samples to read from each files, or -1 for all. Defaults to all
+
 readAllSamples = function (n=-1) {
         rbind(readOneSample('train', n),
               readOneSample('test', n))
 }
 
 library(plyr)
+
+# summarize data set along subject and activity
+# Output: data frame for summary data, with columns
+#   subject: subject id performing activity
+#   activity: activity label as factor, for the activity being performed
+#   meanX, meanY, meanZ, stdX, stdY, stdZ: mean along all samples of mean and standard deviations for X, Y, Z axis
+# Input: data set to summarize
 
 summarize = function (df) {
         ddply(df, .(subject, activity),
@@ -43,8 +68,23 @@ summarize = function (df) {
               stdZ=mean(stdZ))
 }
 
+# compute and dump summary data set into csv summary file
+# Output:
+#   return summary data set
+#   write to disk 'summary.txt' file, which can be read back with read.csv('summary.txt')
+# Input: complete data set to summarize and dump
+
 dumpSummaryFile = function (df) {
         summary = summarize(df)
         write.table(summary, file='summary.txt', row.names=F, col.names=T, sep=',')
+        summary
 }
+
+
+# Main work requested for project:
+
+# Steps 1, 2, 3, 4:
+tidyDataComplete = readAllSamples()
+# Step 5:
+tidyDataSummary = dumpSummaryFile(tidyDataComplete)
 
